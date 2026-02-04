3c33ed4dde96331f7a4753717228b5aff9effc85
Eugene Rwagsore <rwagasore@gmail.com>
2026-02-04 23:57:26 +0200

    chore: remove opencode in favor of pi-coding-agent
    - Remove opencode use-package block and keybindings from config.org
    - Remove opencode package from packages.el

--- doom/packages.el
+++ doom/packages.el
@@ -7,10 +7,6 @@
 ;; Better org-mode bullets - replaces default asterisks with custom bullets
 (package! org-superstar)
 
-;; AI coding assistant - OpenCode integration for chat-based coding
-(package! opencode
-  :recipe (:host codeberg :repo "sczi/opencode.el"))
-
 ;; AI coding assistant - Pi agent for interactive coding sessions
 (package! pi-coding-agent
   :recipe (:host github :repo "dnouri/pi-coding-agent"))
